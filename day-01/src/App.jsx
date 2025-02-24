9f8549ce85391f2c46f53199d87cdf189ab688fb
salman-itShaala <salman.shaikh@itshaala.com>
2025-02-24 16:41:06 +0530

    added day-01 project

---
+++ day-01/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="underline font-bold text-blue-500 text-center">
+      Welcome to 15 days live reactjs project series.
+    </h1>
+  );
+};
+
+export default App;
