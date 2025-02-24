f4f5f4404a7b36857b818cf1d6e27071717f20bd
salman-itShaala <salman.shaikh@itshaala.com>
2025-02-24 16:50:49 +0530

    added reset button

--- day-01/src/App.jsx
+++ day-01/src/App.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [counter, setCoutner] = useState(0);
+  const [counter, setCounter] = useState(0);
 
   return (
     <>
       <h1 className="text-3xl font-bold text-blue-500 ">{counter}</h1>
       <button
         className="border-1 border-slate-700 rounded-sm"
-        onClick={() => setCoutner(counter + 1)}
+        onClick={() => setCounter(counter + 1)}
       >
         Click me
+      </button>
+
+      <button
+        className="ml-4 border-1 border-slate-700 rounded-sm"
+        onClick={() => setCounter(0)}
+      >
+        Reset
       </button>
     </>
   );
