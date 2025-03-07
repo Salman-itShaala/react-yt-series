bacb2450385e0b6f82a2700027811e7151c29a50
salman-itShaala <salman.shaikh@itshaala.com>
2025-03-07 16:57:26 +0530

    added show expense and income in dashboard page

--- expense-tracker/src/components/Home.jsx
+++ expense-tracker/src/components/Home.jsx
@@ -1,30 +1,53 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Home = ({ incomeList, expenseList }) => {
   return (
-    <div className="text-3xl font-bold px-20 py-4">
-      <h1>You will see all your expense here</h1>
+    <div className="px-20 py-4">
+      <h1 className="text-3xl font-bold  py-4">
+        Track your income and expenses.
+      </h1>
 
-      {incomeList.map((income) => {
-        return (
-          <li key={income.id}>
-            {income.incomeTitle} {income.incomeAmount}
-          </li>
-        );
-      })}
+      <div className="flex justify-between w-full">
+        <div className="w-1/2">
+          <h2 className="text-2xl font-bold text-green-500 py-2">Income </h2>
+          <div className="flex gap-4 font-bold py-2">
+            <p className="w-1/3">Sr. No.</p>
+            <p className="w-1/3">Income Title </p>
+            <p className="w-1/3">Income Amount</p>
+          </div>
+          {incomeList.map((income, index) => {
+            return (
+              <div key={income.id} className="flex gap-4">
+                <p className="w-1/3">{index + 1}.</p>
+                <p className="w-1/3"> {income.incomeTitle}</p>
+                <p className="w-1/3"> Rs. {income.incomeAmount} </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-1/2">
+          <h2 className="text-2xl font-bold text-red-500 py-2">Expense </h2>
+
+          <div className="flex gap-4 font-bold py-2">
+            <p className="w-1/3">Sr. No.</p>
+            <p className="w-1/3">Expense Title </p>
+            <p className="w-1/3">Expense Amount</p>
+          </div>
+          {expenseList.map((expese, index) => {
+            return (
+              <div key={expese.id} className="flex gap-4">
+                <p className="w-1/3">{index + 1}.</p>
+                <p className="w-1/3"> {expese.expenseTitle}</p>
+                <p className="w-1/3"> Rs. {expese.expenseAmount} </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
 
       <br />
       <br />
       <br />
-
-      {expenseList.map((expese) => {
-        return (
-          <li key={expese.id}>
-            {expese.expenseTitle} {expese.expenseAmount}
-          </li>
-        );
-      })}
     </div>
   );
 };
